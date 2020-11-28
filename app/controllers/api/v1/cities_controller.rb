e70770b1b415c7ff9e33a38b5948e1ae5e9d6240
Paco Lopez <francisco.lopsan@gmail.com>
2020-11-28 13:50:51 +0100

    Allow CORS

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
 module Api
-    module V1
-        class CitiesController < ApplicationController
-            def create
-                City.create(city_params)
+  module V1
+    class CitiesController < ApplicationController
+      def create
+        City.create(city_params)
 
-                head :created
-            end
+        head :created
+      end
 
-            private
-            def city_params
-                params.require(:city).permit(:name)
-            end
-        end
+      private
+
+      def city_params
+        params.require(:city).permit(:name)
+      end
     end
+  end
 end
